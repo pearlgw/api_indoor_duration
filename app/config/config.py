380d963502d49b2b7c76bb2677e7db4ca8aa0801
pearlgw <gayuhterluv@gmail.com>
2024-12-30 21:11:43 +0700

    update: containerization

--- app/config/config.py
+++ app/config/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("URL_DATABASE")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20, pool_recycle=3600, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
